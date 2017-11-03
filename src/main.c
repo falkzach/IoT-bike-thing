a588df1aaf96ef59052796f0ddaba0ce886fcd9c
Robin Lockwood <rlockwood@localhost.localdomain>
2017-11-02 18:29:05 -0600

    added basic gps code to main.

--- src/main.c
+++ src/main.c
@@ -16,33 +16,110 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "driver/uart.h"
+#include "minmea.h"
 
 #include "./LSM9DS1_accel_gyro_register_map.hpp"
 #include "./LSM9DS1_magnetic_register_map.hpp"
 
+// The GP-20u7 unit is only used to recieve data from the satellite.
+#define GPS_TX_ESP_RX GPIO_NUM_16
+#define GPS_UART_NUM UART_NUM_2
+#define BUF_SIZE (2048)
+
+void parse_gps_rmc(const char * line) {
+
+	switch (minmea_sentence_id(line, false)) {
+		case MINMEA_SENTENCE_RMC: {
+			struct minmea_sentence_rmc rmc_frame;
+			if(minmea_parse_rmc(&rmc_frame, line)) {
+				printf("RMC: raw coordinates and speed: (%d/%d,%d/%d) %d/%d\n",rmc_frame.latitude.value,rmc_frame.latitude.scale,rmc_frame.longitude.value,rmc_frame.longitude.scale,rmc_frame.speed.value,rmc_frame.speed.scale);
+			}
+			break;
+		}
+		case MINMEA_SENTENCE_GLL: {
+			printf("GLL SENTENCE\n");
+			break;
+		}
+		case MINMEA_SENTENCE_VTG: {
+			printf("VTG SENTENCE\n");
+			break;
+		}
+		case MINMEA_SENTENCE_ZDA: {
+			printf("ZDA SENTENCE\n");
+			break;
+		}
+		case MINMEA_SENTENCE_GGA: {
+			printf("GGA SENTENCE\n");
+			break;
+		}
+		case MINMEA_SENTENCE_GSA: {
+			printf("GSA SENTENCE\n");
+			break;
+		}
+		case MINMEA_UNKNOWN: {
+			printf("UNKNOWN SENTENCE\n");
+			break;
+		}
+		case MINMEA_INVALID: {
+			printf("INVALID SENTENCE\n");
+			break;
+		}
+		default: {
+			printf("NONE\n");
+			break;
+		}
+	}
+
+}
+
+// Read gps data from GP-20u7 gps.
+void read_gps(uint8_t *line) {
+  int size; unsigned int count = 0;
+	uint8_t * ptr = line;
+	do {
+		size = uart_read_bytes(GPS_UART_NUM, ptr, 1, 1000/portMAX_DELAY);
+    printf("%d\n",size);
+			if (*ptr == '\n') {
+				ptr++;
+				*ptr = 0;
+				break;
+			}
+			ptr++;
+			++count;
+  } while (size > 0 || count > 256);
+}
+
+// init for the GP-20u7 gps
+void gps_init() {
+	
+	uart_config_t bikeGpsConfig;
+       	
+	bikeGpsConfig.baud_rate           = 9600;
+	bikeGpsConfig.data_bits           = UART_DATA_8_BITS;
+	bikeGpsConfig.parity              = UART_PARITY_DISABLE;
+	bikeGpsConfig.stop_bits           = UART_STOP_BITS_1;
+	bikeGpsConfig.flow_ctrl           = UART_HW_FLOWCTRL_DISABLE;
+	bikeGpsConfig.rx_flow_ctrl_thresh = 120;
+
+	uart_param_config(GPS_UART_NUM, &bikeGpsConfig);
+
+	uart_set_pin(GPS_UART_NUM, UART_PIN_NO_CHANGE, GPS_TX_ESP_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+	uart_driver_install(GPS_UART_NUM, BUF_SIZE, BUF_SIZE, 10, NULL, 0);
+
+}
 
 void app_main()
 {
-    printf("Hello world!\n");
+    gps_init();
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+      char * rmc_line;
+	rmc_line = (char *)malloc(256);
 
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+  while(1) {
+		read_gps((uint8_t *)rmc_line);
+		parse_gps_rmc(rmc_line);
+		read_9dof()
+  }
 }
