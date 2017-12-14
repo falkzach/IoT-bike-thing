2143b41d9165272adb6c7fd23f73102119abb8b6
Robin Lockwood <rlockwood@wl-6-129.wireless.umt.edu>
2017-12-14 13:05:17 -0700

    worked in 9d0f calculations

--- main/bike_thing.cpp
+++ main/bike_thing.cpp
@@ -81,77 +81,16 @@ const byte onLedPin = 26;
 const byte recordLedPin = 27;
 const byte wifiLedPin = 14;
 
-void printGyro() {
-  	Serial.print("G: ");
-	// If you want to print calculated values, you can use the
-  	// calcGyro helper function to convert a raw ADC value to
-  	// DPS. Give the function the value that you want to convert.
-  	Serial.print(imu.calcGyro(imu.gx), 2);
-  	Serial.print(", ");
-  	Serial.print(imu.calcGyro(imu.gy), 2);
-  	Serial.print(", ");
-  	Serial.print(imu.calcGyro(imu.gz), 2);
-  	Serial.println(" deg/s");
-}
-
 void printAccel() {
-	Serial.print("A: ");
-	// If you want to print calculated values, you can use the
-  	// calcAccel helper function to convert a raw ADC value to
-	// g's. Give the function the value that you want to convert.
-  	Serial.print(imu.calcAccel(imu.ax), 2);
-  	Serial.print(", ");
-  	Serial.print(imu.calcAccel(imu.ay), 2);
-  	Serial.print(", ");
-  	Serial.print(imu.calcAccel(imu.az), 2);
-  	Serial.println(" g");
+	Serial.print("Accel: ");
+	Serial.print(imu.calcAccel(imu.ax));Serial.print(", ");
+	Serial.print(imu.calcAccel(imu.ay));Serial.print(", ");
+	Serial.print(imu.calcAccel(imu.az));Serial.print("\n");
 }
-
-void printMag() {
-  	// Now we can use the mx, my, and mz variables as we please.
-  	Serial.print("M: ");
-	// If you want to print calculated values, you can use the
-	// calcMag helper function to convert a raw ADC value to
-	// Gauss. Give the function the value that you want to convert.
-	Serial.print(imu.calcMag(imu.mx), 2);
-	Serial.print(", ");
-	Serial.print(imu.calcMag(imu.my), 2);
-	Serial.print(", ");
-	Serial.print(imu.calcMag(imu.mz), 2);
-	Serial.println(" gauss");
-}
-
-// Calculate pitch, roll, and heading.
-// Pitch/roll calculations take from this app note:
-// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
-// Heading calculations taken from this app note:
-// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz) {
-  	float roll = atan2(ay, az);
-  	float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-
-  	float heading;
-  	if (my == 0)
-   		heading = (mx < 0) ? PI : 0;
-  	else
-    		heading = atan2(mx, my);
-
-  	heading -= DECLINATION * PI / 180;
-
-  	if (heading > PI) heading -= (2 * PI);
-  	else if (heading < -PI) heading += (2 * PI);
-  	else if (heading < 0) heading += 2 * PI;
-
-  	// Convert everything from radians to degrees:
-  	heading *= 180.0 / PI;
-  	pitch *= 180.0 / PI;
-  	roll  *= 180.0 / PI;
-
-  	Serial.print("Pitch, Roll: ");
-  	Serial.print(pitch, 2);
-  	Serial.print(", ");
-  	Serial.println(roll, 2);
-  	Serial.print("Heading: "); Serial.println(heading, 2);
+void writeAccel() {
+	char g_forces[128];
+	sprintf(g_forces, "%f,%f,%f\n", imu.calcAccel(imu.ax), imu.calcAccel(imu.ay), imu.calcAccel(imu.ax));
+	appendFile(SD, "/data/gforce.txt", g_forces);
 }
 
 void print_html(WiFiClient client) {
@@ -253,8 +192,8 @@ void setup(){
 	pinMode(wifiLedPin, OUTPUT);
 	digitalWrite(wifiLedPin, LOW);	
 	// set up serial ports
-    	//Serial.end();
-	//Serial.flush();
+    	Serial.end();
+	Serial.flush();
 	Serial.begin(115200);
 	gps_serial.begin(9600);
 
@@ -291,6 +230,7 @@ void setup(){
 		}
 		//return;
     	}
+	writeFile(SD, "/data/gforce.txt", " ");
 	build_kml_header();
 
 }
@@ -309,6 +249,7 @@ void setup_wifi() {
 		delay(500);
 		Serial.print(".");
 	}
+
 	if (WiFi.status() == WL_CONNECTED) {
 		Serial.println("");
 		Serial.println("WiFi connected");
@@ -389,55 +330,32 @@ void run_sensors() {
 			Serial.println(coordinates);
 			appendFile(SD, "/html/bikeride.kml",coordinates);
 	
-			// Update the sensor values whenever new data is available
-			if ( imu.gyroAvailable() ) {
-				// To read from the gyroscope,  first call the
-				// readGyro() function. When it exits, it'll update the
-				// gx, gy, and gz variables with the most current data.
-				//imu.readGyro();
-			}
 			if ( imu.accelAvailable() ) {
-				// To read from the accelerometer, first call the
-				// readAccel() function. When it exits, it'll update the
-				// ax, ay, and az variables with the most current data.
-				//imu.readAccel();
-			}
-			if ( imu.magAvailable() ) {
-				// To read from the magnetometer, first call the
-				// readMag() function. When it exits, it'll update the
-				// mx, my, and mz variables with the most current data.
-				//imu.readMag();
+				imu.readAccel();
 			}
 	
 			if ((lastPrint + PRINT_SPEED) < millis()) {
-				//printGyro();  // Print "G: gx, gy, gz"
-				//printAccel(); // Print "A: ax, ay, az"
-				//printMag();   // Print "M: mx, my, mz"
-				// Print the heading and orientation for fun!
-				// Call print attitude. The LSM9DS1's mag x and y
-				// axes are opposite to the accelerometer, so my, mx are
-				// substituted for each other.
-				//printAttitude(imu.ax, imu.ay, imu.az,
-	        		//    -imu.my, -imu.mx, imu.mz);
-				//Serial.println();
+				writeAccel(); // Print "A: ax, ay, az"
 	
 				lastPrint = millis(); // Update lastPrint time
 			}
-      			smartdelay(1000);
 		}
 	}
+	if (imu.accelAvailable() ) {
+		imu.readAccel();
+		printAccel();
+	}
+	smartdelay(1000);
+
 }
 
 static void smartdelay(unsigned long ms) {
 	unsigned long start = millis();
-	do {
-		while (gps_serial.available()) {
-			const char c = gps_serial.read();
-			Serial.print(c);
-			appendFile(SD, "/data/gps_data.txt",&c);
-			gps.encode(c);
-		}
-	} while (millis() - start < ms);
+	while (gps_serial.available() && (millis()-start<ms)) {
+		const char c = gps_serial.read();
+		appendFile(SD, "/data/gps_data.txt",&c);
+		gps.encode(c);
+	} 
 }
 
 void loop(){
